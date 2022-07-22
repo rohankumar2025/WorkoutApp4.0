7e13986a45402c1c334aed83dbc6bbd30cdc019f
Rohan Kumar <rohankumar@ucsb.edu>
2022-07-22 11:16:34 -0700

    Initial Commit

---
+++ WorkoutApp4.0/WorkoutApp4_0App.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutApp4_0App.swift
+//  WorkoutApp4.0
+//
+//  Created by Rohan Kumar on 7/22/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutApp4_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
